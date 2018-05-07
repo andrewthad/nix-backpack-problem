78945c3e4f4ab3c91751970a6cbe23b42336f6a5
Andrew Martin <andrew.thaddeus@gmail.com>
2018-05-07 09:17:35 -0400

    cause problem to happen with nix-build as well

--- Main.hs
+++ Main.hs
@@ -2,5 +2,18 @@
 
 module Main where
 
+import qualified GHC.Exts as E
+import qualified Data.Map.Unboxed.Unboxed as M
+import qualified Data.Map.Unboxed.Lifted as ML
+
+value :: M.Map Int Int
+value = E.fromList [(6,100),(2,9)]
+
+valueX :: ML.Map Int Int
+valueX = E.fromList [(7,100),(18,11)]
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  putStrLn "Hello, world!"
+  print value
+  print valueX
